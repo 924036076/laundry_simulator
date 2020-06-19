f64a3e2c60b853c97b49e73e64d0f6a643ec363c
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2020-06-19 13:27:57 -0700

    made washer shake on unallowed action

--- models/washer/Washer.gd
+++ models/washer/Washer.gd
@@ -1,32 +1,47 @@
 extends "res://models/interactable/Interactable.gd"
-	
+
+var state_machine : AnimationNodeStateMachinePlayback
+
 func _ready():
 	._ready()
 	offset = Vector2(0,5)
+	state_machine = $AnimationTree["parameters/StateMachine/playback"]
+	state_machine.start("idle")
+	$AnimationTree.set("parameters/Blend/blend_amount", 0)
 	
 func load_laundry(laundry_in):
 	.load_laundry(laundry_in)
 	if !laundry:
 		return
 	laundry.position = offset
-	if laundry.can_wash():
-		$AnimationPlayer.play("running")
-		laundry.visible = false
-		interactable = false
-		laundry.wash()
-		$Timer.start()
-		yield($Timer, "timeout")
+	if can_run():
+		_start_load()
+		_change_laundry_state()
+
+func _change_laundry_state():
+	laundry.wash()
+	
+func can_run() -> bool:
+	return laundry.can_wash()
+	
+func _start_load():
+	laundry.visible = false
+	interactable = false
+	state_machine.travel("running")
+	$Timer.start()
 
 func _finish_load():
-	$AnimationPlayer.play("idle")
+	state_machine.travel("idle")
 	laundry.visible = true
 	interactable = true
 	
 func reset():
 	.reset()
-	$AnimationPlayer.play("idle")
+	state_machine.travel("idle")
 	$Timer.stop()
 	interactable = true
 	
 func disallowed_action():
-	$AnimationPlayer.play("shake")
+	$AnimationTree.set("parameters/Blend/blend_amount", 0.5)
+	yield(get_tree().create_timer(0.4), "timeout")
+	$AnimationTree.set("parameters/Blend/blend_amount", 0)
--- music/BackgroundMusic.gd
+++ music/BackgroundMusic.gd
@@ -1,5 +1,12 @@
 extends Node
+var master_volume : float = -8
 
+func _ready():
+	$SongIntro.set("volume_db", master_volume)
+	$SongLoop.set("volume_db", master_volume)
+	
 func restart():
-	$SongLoop.stop()
+	# Stops the song loop (if playing) and starts the intro
+	# Song Loop listens for the intro to finish, then plays and loops indefinitely
+	$SongLoop.stop() 
 	$SongIntro.play()
