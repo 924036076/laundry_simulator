8440b706d2567d936791c7976f0aac783a0c4649
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2020-07-28 20:13:16 -0700

    added laundry holder to customer

--- characters/customers/base_customer/customer.gd
+++ characters/customers/base_customer/customer.gd
@@ -17,6 +17,7 @@ var received_laundry := false
 signal leaving
 signal returning
 
+
 func _ready() -> void:
 	._ready()
 	set_physics_process(false)
@@ -29,11 +30,13 @@ func _ready() -> void:
 	EventHub.connect("patience_cloud", self, "on_patience_cloud")
 	$PatienceMeter.set_max_patience(max_patience)
 
+
 func init(node : Navigation2D, id : int, wait_time : float) -> void:
 	navNode = node
 	set_laundry_id(id)
 	return_destination = global_position
 	$Timer.set_wait_time(wait_time)
+
 
 func set_laundry_id(id : int) -> void:
 	if my_laundry:
@@ -41,6 +44,7 @@ func set_laundry_id(id : int) -> void:
 		$Bumper/HandPos/Ticket/Label.text = str(id)
 	else:
 		print_debug("ERROR: no laundry to give id")
+	
 	
 func drop_off() -> void:
 	set_physics_process(false)
@@ -50,6 +54,7 @@ func drop_off() -> void:
 	$Timer.start()	
 	leave_store()
 	$PatienceMeter.on_drop_off()
+	
 	
 func receive_order() -> void:
 	$Bumper.interactable = false
@@ -65,20 +70,24 @@ func receive_order() -> void:
 	received_laundry = true
 	leave_store()
 	
+	
 func happy_buff() -> void:
 	# To be overridden by inherited customers
 	pass
+	
 	
 func leave_store() -> void:
 	set_target_location(return_destination)
 	EventHub.emit_signal("customer_leaving", self)
 	#$Bumper.monitoring = false
 
+
 func assess_laundry() -> void:
 	if $Bumper.laundry == my_laundry:
 		cleanliness_pct = my_laundry.assess_cleanliness()
 		score = cleanliness_pct * score_multiplier * $PatienceMeter.get_patience()
 	$PatienceMeter.hide()
+
 
 func emote(happiness_pct: float) -> AnimatedSprite:
 	var expression = preload("res://characters/emote/emote.tscn").instance()
@@ -87,6 +96,7 @@ func emote(happiness_pct: float) -> AnimatedSprite:
 	expression.set_and_play(happiness_pct)
 	return expression
 	
+	
 func storm_off() -> void:
 	$PatienceMeter.hide()
 	if score == 0:
@@ -94,8 +104,10 @@ func storm_off() -> void:
 		yield(expression, "animation_finished")
 	leave_store()
 	
+	
 func _on_Timer_timeout() -> void:
 	back_to_store()
+	
 	
 func back_to_store() -> void:
 	EventHub.emit_signal("customer_entering", self)
@@ -103,25 +115,31 @@ func back_to_store() -> void:
 	$Bumper.interactable = true
 	$Bumper.set_state_pickup()
 	
+	
 func last_call() -> void:
 	$Timer.stop()
 	back_to_store()
+
 
 func decrement_patience() -> void:
 	if received_laundry or walking: 
 		return
 	modify_patience_points(-1)
 
+
 func _on_Bumper_disallowed_customer_action() -> void:
 	$AnimationPlayer.play("shake")
+	
 	
 func _on_end_of_path() -> void:
 	._on_end_of_path()
 	if global_position.distance_to(return_destination) <= 5 and received_laundry:
 		queue_free()
 
+
 func _on_Bumper_modulate(modulation : Color) -> void:
 	$Sprite.modulate = modulation
+	
 	
 func modify_patience_points(points : int) -> void:
 	var addition := points * patience_unit
